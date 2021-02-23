5d7a2cea8710670cc81d4e4864387d1565718957
Ignas Lapėnas <ignas@lapenas.dev>
2021-02-23 12:44:11 +0200

    add index test template

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,30 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::templates::Template;
+use std::collections::HashSet;
+use serde::Serialize;
+
+#[get("/hello")]
+fn hello() -> String {
+    format!("Hello_world")
+}
+
+#[get("/template")]
+fn template() -> Template {
+
+    let context = Context{};
+    Template::render("index", &context)
+}
+
+#[derive(Serialize)]
+struct Context {}
+
 fn main() {
     println!("Hello, world!");
+
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/", routes![hello, template])
+        .launch();
 }
